eb382fab586735790af749a2b3ecc50af36f45e5
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2025-10-08 13:45:14 +0200

    add about dynamic name and version

--- app/src/main/java/com/example/flashcard/AboutActivity.java
+++ app/src/main/java/com/example/flashcard/AboutActivity.java
@@ -1,9 +1,9 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +20,17 @@ public class AboutActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String appName = getString(R.string.app_name);
+
+        ((TextView) findViewById(R.id.appText)).setText(appName);
+
+        try {String version = getPackageManager().getPackageInfo(
+                    getPackageName(), 0).versionName;
+
+            ((TextView) findViewById(R.id.versionText)).setText("Version: " + version);
+        } catch (Exception e) {
+            ((TextView) findViewById(R.id.versionText)).setText("Version: Inconnu");
+        }
     }
 }
